40ae052f5499e335b03e1021bc4ca983ab05b86b
hmschult1 <hmschult1@geneva.edu>
2026-07-13 12:50:19 -0400

    Change UPLOAD_FOLDER to CMS location placeholder
    Updated the UPLOAD_FOLDER configuration to indicate a placeholder for CMS location.

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app
 
 app = create_app()
 
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = # ADD CMS LOCATION HERE
 
 if __name__ == '__main__':
     app.run(debug=True)
